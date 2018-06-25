347c6645c0d424553c0cbd531667d043cb23a399
Zhuanglijuan <1330129868@qq.com>
2018-06-25 23:50:17 +0800

    增加J.U.C之AQS部分

---
+++ src/main/java/com/zhuang/concurrency/example/aqs/CyclicBarrierExample1.java
@@ -0,0 +1,40 @@
+package com.zhuang.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class CyclicBarrierExample1 {
+
+    private static CyclicBarrier barrier = new CyclicBarrier(5);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for(int i = 0 ; i < 10 ; i ++){
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executor.execute(()->{
+                try {
+                    race(threadNum);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    private static void race(int threadNum) throws InterruptedException, BrokenBarrierException {
+        Thread.sleep(1000);
+        log.info("{} is ready", threadNum);
+        barrier.await();
+        log.info("{} continue",threadNum);
+    }
+}
---
+++ src/main/java/com/zhuang/concurrency/example/lock/LockExample5.java
@@ -0,0 +1,60 @@
+package com.zhuang.concurrency.example.lock;
+
+import com.zhuang.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.StampedLock;
+
+
+/**
+ * 并发模拟
+ */
+@Slf4j
+@ThreadSafe
+public class LockExample5 {
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+
+    public static int count = 0;
+
+    private final static StampedLock lock = new StampedLock();
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for(int i = 0 ; i < clientTotal ; i ++){
+            executorService.execute(() ->{
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("exception",e);
+                }
+            });
+            countDownLatch.countDown();
+        }
+
+        //保证countDown减为0，所有进程都执行完
+        countDownLatch.await();
+        //关闭线程池
+        executorService.shutdown();
+        log.info("count :{}" ,count);
+    }
+
+    private static void add(){
+        long stamp = lock.writeLock();
+        try{
+            count++;
+        }finally {
+            lock.unlock(stamp);
+        }
+    }
+}
